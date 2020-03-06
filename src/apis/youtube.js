c688a253883b11c7206fc7986369249506ff6904
Andrew Chan <andrew.chan@baygullstudios.com>
2020-03-06 15:41:48 -0600

    removed API key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,13 +1,11 @@
 import axios from 'axios'
 
-const KEY = 'AIzaSyDxvbVy5zeebjFjQCJ3xh1WemA08pEOpGw'
-
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
     maxResults: 5,
     type: 'video',
-    key: KEY
+    key: process.env.REACT_APP_API_KEY
   }
 })
